9acf9d8eb1ff9dc4ac4ce571a51b2200237e44ed
PhungNV <phungnv.bkdn@gmail.com>
2019-09-08 17:15:25 +0700

    [SEO] description

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -10,6 +10,8 @@ const BaseLayout = (props) => {
     <React.Fragment>
       <Head>
         <title>{title}</title>
+        <meta name="description" content="My name is Phung Nguyen and I am an experienced web developer. I have a Bachelor's degree in Software Engineering and several years of experience working in web development. Throughout my career, I have acquired advanced technical knowledge and the ability to explain programming topics clearly and in detail to a broad audience. I hope to have a change to work with you one day." />
+        <meta name="keywords" content="phungnv profile, phungnv blog, phungnv portfolio, phungnv developer, phungnv freelancer, phungnv programming" />
         <script src="https://kit.fontawesome.com/494dbe5a09.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
       </Head>
