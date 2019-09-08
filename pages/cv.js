98d84f347833ad4d553b15bbb9a1ba425d6df2ec
PhungNV <phungnv.bkdn@gmail.com>
2019-09-08 16:12:01 +0700

    improve CV page

--- pages/cv.js
+++ pages/cv.js
@@ -2,13 +2,25 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 
+import { Row, Col } from 'reactstrap';
+
 class Cv extends React.Component {
 
   render() {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage>
-          <h1> I am Cv Page </h1>
+        <BasePage title="Preview of my CV" className="cv-page">
+          <Row>
+            <Col md={{ size: 8, offset: 2 }}>
+              <div className="cv-title">
+                <a download="phung_cv.pdf" className="btn btn-success" href="/static/phung_cv.pdf">
+                  Download
+                </a>
+              </div>
+              <iframe style={{ width: '100%', height: '800px' }} src="/static/phung_cv.pdf">
+              </iframe>
+            </Col>
+          </Row>
         </BasePage>
       </BaseLayout>
     )
