99a9a2bf44859b209b3678e2a97a4a8752c8b1c8
PhungNV <phungnv.bkdn@gmail.com>
2019-09-08 23:10:06 +0700

    add social links

--- pages/blogs.js
+++ pages/blogs.js
@@ -74,15 +74,7 @@ class Blogs extends React.Component {
                 <div className="col-lg-8 col-md-10 mx-auto">
                   <ul className="list-inline text-center">
                     <li className="list-inline-item">
-                      <a href="#">
-                        <span className="fa-stack fa-lg">
-                          <i className="fas fa-circle fa-stack-2x"></i>
-                          <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                        </span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
+                      <a target="_blank" href="https://www.facebook.com/phung.76">
                         <span className="fa-stack fa-lg">
                           <i className="fas fa-circle fa-stack-2x"></i>
                           <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -90,7 +82,7 @@ class Blogs extends React.Component {
                       </a>
                     </li>
                     <li className="list-inline-item">
-                      <a href="#">
+                      <a target="_blank" href="https://github.com/phungnv">
                         <span className="fa-stack fa-lg">
                           <i className="fas fa-circle fa-stack-2x"></i>
                           <i className="fab fa-github fa-stack-1x fa-inverse"></i>
@@ -98,7 +90,7 @@ class Blogs extends React.Component {
                       </a>
                     </li>
                   </ul>
-                  <p className="copyright text-muted">Copyright &copy; Filip Jerga 2018</p>
+                  <p className="copyright text-muted">Copyright &copy; PhungNV 2019</p>
                 </div>
               </Row>
             </Container>
