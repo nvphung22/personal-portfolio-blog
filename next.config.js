059fe63824bded49c123b43ec21e6ac960be8e7b
PhungNV <phungnv.bkdn@gmail.com>
2019-12-04 09:36:07 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withCSS = require('@zeit/next-css');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = withCSS(withSass({
-    webpack(config, { dev }) {
+    webpack: config => {
         if (config.mode === 'production') {
             if (Array.isArray(config.optimization.minimizer)) {
                 config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
